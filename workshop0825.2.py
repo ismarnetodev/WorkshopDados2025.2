aca024945db675589867df68cc09b6593ab37852
ismarnetodev <dsxzz41@gmail.com>
2025-09-08 23:29:47 -0300

    atividade do workshop de dados 2025.2

---
+++ workshop0825.2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random as rd
+
+temperatura = rd.sample(range(20, 35), 7)
+
+temperatura_array = np.array(temperatura)
+
+dia_da_semana = ["segunda", "terça", "quarta", "quinta", "sexta", "sabado", "domingo"]
+
+
+data = {
+    'Dias da semana': dia_da_semana,
+    "Temperatura (°C)": temperatura
+    }
+
+df= pd.DataFrame(data)
+
+media = np.mean(temperatura_array)
+print("\ntemperatura media da semnana", media)
+print("temperatura maxima:", np.max(temperatura_array))
+print("temperatura minima:", np.max(temperatura_array))
+
+plt.plot(dia_da_semana, temperatura, marker='o', label='Variação da temperatura em João Pessoa')
+plt.xlabel('dias da semana')
+plt.ylabel('temperatura')
+plt.title('variação da temperatura')
+plt.legend()
+plt.grid(True)
+plt.show()
